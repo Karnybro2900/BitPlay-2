ffb4990d7b4fb94e885e4fc92f2bdaa1e7dc7e2b
Karnybro2900 <rinmadh27@gmail.com>
2025-07-30 18:31:25 +0530

    hi

---
+++ a.py
@@ -0,0 +1,8 @@
+#Check whetther the two numbers are odd or not
+n1=int(input("Enter n1 : "))
+n2=int(input("Enter n2 : "))
+if n1^n2==0:
+    print(n1," and ",n2," are not equal numbers")
+else:
+    print(n1," and ",n2," are equal numbers")
+
